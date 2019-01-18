b0b4883d3fc59becaa3e77048bd3fe6a41c573a0
IceCreamWW <1450648595@qq.com>
2019-01-18 15:42:56 +0800

    v2.0 autoplay, thumbnail-list background

--- js/demonstration.js
+++ js/demonstration.js
@@ -2,23 +2,43 @@
     document.addEventListener("fullscreenchange", function() {
         location.reload()
     });
-    $('#fullpage').fullpage();
+    $('#fullpage').fullpage({
+        onLeave: function(origin, destination, direction){
+            if (destination.index === 1) {
+                $(".thumbnails-list").slick('slickPlay');
+            }
+            if (origin.index === 1){
+                $(".thumbnails-list").slick('slickPause');
+            }
+        }
+    });
     $('.depicts-list').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: false,
         fade: true,
+        pauseOnHover: true,
         asNavFor: '.thumbnails-list',
         draggable: false,
     });
 
     $(".thumbnails-list").slick({
         arrows: false,
-        centerMode: false,
-        infinite: false,
+        centerMode: true,
+        infinite: true,
         slidesToShow: 5,
         focusOnSelect: true,
         swipeToSlide: true,
+        autoplay: false,
+        autoplaySpeed: 2000,
         asNavFor: '.depicts-list',
     });
+    $('.depict-image, .thumbnails-list').hover(
+        function () {
+            $(".thumbnails-list").slick('slickPause');
+        },
+        function () {
+            $(".thumbnails-list").slick('slickPlay');
+        }
+    );
 })(jQuery);
